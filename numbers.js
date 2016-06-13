f060aab45455add52472496c5766ebaba8cfde6a
Marcel Klehr <mklehr@gmx.net>
2016-06-13 15:12:12 +0200

    First pass at implementing LSEQ
    no margins, yet, only base doubling and alternating strategy

--- numbers.js
+++ numbers.js
@@ -7,30 +7,44 @@ module.exports = function (lo, hi) {
   if(compare(lo, hi) >= 0)
     throw new Error('numarray-between: lo must be smaller than hi, was:' +
       JSON.stringify(lo) + ', ' + JSON.stringify(hi))
-  var mid = lo.slice()
-  if(lo.length == hi.length) {
-    if(last(lo) + 1 == last(hi)) //between([1], [2]) => [1, 1]
-      mid.push(1)
+  
+  var i, mid=[], base=36
 
-//was experimenting with "leaving space"
-//for other
-    else if(last(mid) + 10 < last(hi))
-      mid[mid.length-1] += 5
-    else
-      mid[mid.length-1] ++
+  // find common prefix
+  for(i=0; i<lo.length && i<hi.length; i++) {
+    if (lo[i] != hi[i]) break;
+    mid.push(lo[i])
+    base *= 2 // double base for every level in identifier tree
   }
-  else if(lo.length > hi.length)
-    mid[mid.length-1] += 5
-  else if(lo.length < hi.length) {
-    if(last(lo) === hi[lo.length-1]) {
-      for(var i = lo.length; i < hi.length; i++) {
-        mid.push(0)
-      }
-      mid.push(1)
-    }
-    else mid[mid.length-1]++
+
+  var _lo = lo[i], _hi = hi[i]
+
+  if (!_lo) _lo = 0
+  if ('undefined' === _hi) _hi = base
+  
+  while (_lo == base || _lo+1 >= _hi) {
+    // _lo is last possible value in base -> new level in tree
+    // or
+    // _lo and _hi are adjacent or equal -> new level in tree
+    mid.push(_lo)
+    base *= 2
+    i++
+    _lo = lo[i] || 0
+    _hi = 'undefined' !== typeof hi[i]? hi[i] : base
   }
-  return mid
+
+  var strategy = Math.round(Math.random())
+  if (_lo || strategy) {
+    // strategy:append
+    mid.push(_lo+1)
+  }else {
+    mid.push(_hi-1)
+  }
+ 
+  if (compare(lo, mid) < 0 && compare(mid, hi) < 0 && mid[mid.length-1] != 0)
+    return mid
+  
+  throw new Error('ERR_PEBKAC: Developer messed up')
 }
 
 function compareNums (a, b) {
